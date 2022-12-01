17cbfa797f2a0a3c80a2b27ed15963d0f173dd52
Yushun Zeng <d18130495@mytudublin.ie>
2022-12-01 21:22:50 +0000

    Added map

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,16 +1,53 @@
 <template>
-  <div>
+  <div id="text" class="text">
+      <div id="map"></div>
   </div>
 </template>
 
 <script>
+import $ from "jquery"
+import L from 'leaflet'
+import "leaflet/dist/leaflet.css"
+
 import '@/assets/css/index.css'
+
+
+const options = {
+  center: [53.34, -6.27],
+  minZoom: 0,
+  maxZoom: 18,
+  zoom: 12.5,
+  zoomControl: true,
+  doubleClickZoom: true,
+  dragging: true,
+  boxZoom: true,
+  scrollWheelZoom: true,
+  zoomSnap: 0.5,
+  fullscreenControl: true,
+  // attributionControl: true,
+  attribution: '<a target="_blank" href="https://www.openstreetmap.org/">© OpenStreetMap Yushun Zeng</a>'
+}
 
 export default {
   name: "Map",
   data() {
     return {
+      osmMap: {}
     }
+  },
+  mounted() {
+    $("#text").css({
+      "top": $("#header").height() + 10, "position": "absolute"
+    });
+
+    $("#map").css({
+      "width": "100%",
+      "height": $(document).height() - ($("#header").height() + $("#footer").height() + 45)
+    });
+
+    this.osmMap = L.map('map', options)
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', options).addTo(this.osmMap)
   },
   created() {
   },
